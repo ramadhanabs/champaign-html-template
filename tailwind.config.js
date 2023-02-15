5c1f5fb2d08f6f94618a32fd6f3521a55571bf0c
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-02-15 17:41:27 +0700

    feat: slicing homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'], // add your preferred file extensions here
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9D22C2",
+        "primary-purple": "#7A1AD6",
+        "grayscale-10": "#fdfdfd",
+        "grayscale-20": "#ECF1F6",
+        "grayscale-30": "#E3E9ED",
+        "grayscale-40": "#D1D8DD",
+        "grayscale-50": "#BFC6CC",
+        "grayscale-60": "#9CA4AB",
+        "grayscale-70": "#78828A",
+        "grayscale-80": "#66707A",
+        "grayscale-90": "#434E58",
+        "grayscale-100": "#070717",
+      },
+    },
   },
   plugins: [],
-};
+}
